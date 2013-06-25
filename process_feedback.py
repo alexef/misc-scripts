e6d0ac7b9c0a4699f6f188e4a01b2751e0dc5fd8
Alex Eftimie <alex@eftimie.ro>
2013-06-25 16:53:27 +0300

    Process txt feedback

---
+++ process_feedback.py
@@ -0,0 +1,35 @@
+import re
+import csv
+import os
+
+def process_file(filename):
+    if not os.path.exists(filename):
+        return None
+    fin = open(filename, 'r')
+    lines = fin.readlines()
+    answers = []
+    mode = 0
+    answer = None
+    for l in lines:
+        if l[0] == '\xef': 
+            l = l[3:]
+        if re.match(r'[0-9]+\.', l) or l.startswith('1. '):
+            if answer is not None:
+                answers.append(answer.strip())
+            answer = ''
+        else:
+            answer += l
+    return answers
+
+data = []
+for i in range(0, 16):
+    a = process_file('%02d.txt' % i)
+    if a:
+        data.append(a)
+
+with open('result.csv', 'w') as fout:
+    writer = csv.writer(fout)
+    for d in data:
+        writer.writerow(d)
+
+print "Done!"
